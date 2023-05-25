90d4dadfaa206273873fb2df90775ba02d2416cc
Your Name <richardvieiralopes@gmail.com>
2023-05-25 20:23:12 -0300

    sector added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.querySelector(".burguer").addEventListener('click', () => {
     document.querySelector("#menu").classList.toggle("active")
     document.querySelector(".burguer").classList.toggle("active")
-})
+})  
